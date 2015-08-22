bde06a8605503f62fbab84a9b4ffc9d13c595cfd
Itsara Wichakam <iwichakam@hotmail.com>
2015-08-22 20:55:53 +0700

    Test #2

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "Hello GitHub";
+		return "";
 	}
 	
 	public static int[] bubbleSort(int[] array){
